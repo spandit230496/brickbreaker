29fd851fc8254cfc300d638d761da1cc7f3b2c66
spandit230496 <117799882+spandit230496@users.noreply.github.com>
2022-11-24 08:25:25 +0530

    Update CalculatorVis.java

--- calculatorVis/src/main/java/com/mycompany/calculatorvis/CalculatorVis.java
+++ calculatorVis/src/main/java/com/mycompany/calculatorvis/CalculatorVis.java
@@ -15,11 +15,12 @@ public class CalculatorVis implements ActionListener{
 	JTextField textfield;
 	JButton[] numberButtons = new JButton[10];
 	JButton[] functionButtons = new JButton[9];
+         
 	JButton addButton,subButton,mulButton,divButton;
 	JButton decButton, equButton, delButton, clrButton, negButton;
 	JPanel panel;
 	
-	Font myFont = new Font("Ink Free",Font.BOLD,30);
+	Font myFont = new Font("serif",Font.BOLD,30);
 	
 	double num1=0,num2=0,result=0;
 	char operator;
@@ -30,31 +31,58 @@ public class CalculatorVis implements ActionListener{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420, 550);
 		frame.setLayout(null);
+                frame.setBackground(Color. black);
 		
 		textfield = new JTextField();
 		textfield.setBounds(50, 25, 300, 50);
 		textfield.setFont(myFont);
 		textfield.setEditable(false);
+                textfield.setBackground(Color. black);
+		
 		
 		addButton = new JButton("+");
+                  addButton.setBackground(Color.red);
 		subButton = new JButton("-");
+                    subButton.setBackground(Color.red);
 		mulButton = new JButton("*");
+                     mulButton.setBackground(Color.red);
 		divButton = new JButton("/");
+                    divButton.setBackground(Color.red);
 		decButton = new JButton(".");
+                    decButton.setBackground(Color.red);
 		equButton = new JButton("=");
+                    equButton.setBackground(Color.red);
 		delButton = new JButton("Del");
 		clrButton = new JButton("Clr");
 		negButton = new JButton("(-)");
 		
 		functionButtons[0] = addButton;
-		functionButtons[1] = subButton;
+                  functionButtons[0].setBackground(Color.black);
+                  functionButtons[0].setForeground(Color.white);
+                functionButtons[1] = subButton;
+                  functionButtons[1].setBackground(Color.black);
+                  functionButtons[1].setForeground(Color.white);
 		functionButtons[2] = mulButton;
+                  functionButtons[2].setBackground(Color.black);
+                  functionButtons[2].setForeground(Color.white);
 		functionButtons[3] = divButton;
+                  functionButtons[3].setBackground(Color.black);
+                  functionButtons[3].setForeground(Color.white);
 		functionButtons[4] = decButton;
+                  functionButtons[4].setBackground(Color.black);
+                  functionButtons[4].setForeground(Color.white);
 		functionButtons[5] = equButton;
+                  functionButtons[5].setBackground(Color.black);
+                  functionButtons[5].setForeground(Color.white);
 		functionButtons[6] = delButton;
+                  functionButtons[6].setBackground(Color.black);
+                  functionButtons[6].setForeground(Color.white);
 		functionButtons[7] = clrButton;
+                  functionButtons[7].setBackground(Color.black);
+                  functionButtons[7].setForeground(Color.white);
 		functionButtons[8] = negButton;
+                  functionButtons[8].setBackground(Color.black);
+                  functionButtons[8].setForeground(Color.white);
 		
 		for(int i =0;i<9;i++) {
 			functionButtons[i].addActionListener(this);
@@ -78,6 +106,7 @@ public class CalculatorVis implements ActionListener{
 		panel.setLayout(new GridLayout(4,4,10,10));
 
 		panel.add(numberButtons[1]);
+               
 		panel.add(numberButtons[2]);
 		panel.add(numberButtons[3]);
 		panel.add(addButton);
